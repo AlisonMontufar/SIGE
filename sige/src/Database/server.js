2dd47fef839969a4c5693fce511e1112750bc30b
Emmanuel <emacal200901@gmail.com>
2025-03-18 23:03:47 -0600

    bcrypt

--- sige/src/Database/server.js
+++ sige/src/Database/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const { connectToDatabase, sql } = require('./db'); // Importar conexión a la base de datos
 const app = express();
 const port = 5000;
+const bcrypt = require('bcrypt');
 
 // Configuración de CORS
 app.use(cors());
@@ -26,42 +27,44 @@ app.get('/get-roles', async (req, res) => {
 
 // Ruta para autenticar a un usuario
 app.post('/login', async (req, res) => {
-  const { matricula, contraseña } = req.body;
-
-  try {
-    const pool = await connectToDatabase();
-    const result = await pool.request()
-      .input('matricula', sql.NVarChar, matricula)
-      .query('SELECT * FROM Usuarios WHERE matricula = @matricula');
-
-    // Verificar si el usuario existe
-    if (result.recordset.length === 0) {
-      return res.status(400).json({ message: 'Usuario no encontrado' });
+    const { matricula, contraseña } = req.body;
+  
+    try {
+      const pool = await connectToDatabase();
+      const result = await pool.request()
+        .input('matricula', sql.NVarChar, matricula)
+        .query('SELECT * FROM Usuarios WHERE matricula = @matricula');
+  
+      // Verificar si el usuario existe
+      if (result.recordset.length === 0) {
+        return res.status(400).json({ message: 'Usuario no encontrado' });
+      }
+  
+      const user = result.recordset[0];
+  
+      // Comparar la contraseña encriptada
+      const isMatch = await bcrypt.compare(contraseña, user.contraseña);
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Contraseña incorrecta' });
+      }
+  
+      // Enviar respuesta si la autenticación es exitosa
+      res.json({
+        message: 'Autenticación exitosa',
+        user: {
+          id: user.usuario_id,
+          nombre: user.nombre,
+          rol_id: user.rol_id,
+        },
+      });
+    } catch (error) {
+      console.error('Error en el proceso de autenticación:', error);
+      res.status(500).json({ message: 'Error en el proceso de autenticación', error });
+    } finally {
+      sql.close();
     }
+  });
 
-    const user = result.recordset[0];
-
-    if (contraseña !== user.contraseña) {
-      return res.status(400).json({ message: 'Contraseña incorrecta' });
-    }
-
-    res.json({
-      message: 'Autenticación exitosa',
-      user: {
-        id: user.usuario_id,
-        nombre: user.nombre,
-        rol_id: user.rol_id,
-      },
-    });
-  } catch (error) {
-    console.error('Error en el proceso de autenticación:', error);
-    res.status(500).json({ message: 'Error en el proceso de autenticación', error });
-  } finally {
-    sql.close();
-  }
-});
-
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
