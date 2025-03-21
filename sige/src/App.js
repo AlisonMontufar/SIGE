e3fa6eece81ca0541d032250a353d243a486ca98
Emmanuel <emacal200901@gmail.com>
2025-03-20 18:55:38 -0600

    Arreglando sus Pendejadas del dani y el javi

--- sige/src/App.js
+++ sige/src/App.js
@@ -1,22 +1,26 @@
-import Login from './components/Auth/Login';
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react';  
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
-
 import './App.css';
+import Login from './components/Auth/Login';
+import Home from './components/PortalInicial/home'
+
+function Homero() {
+  const navigate = useNavigate();  
+
+  useEffect(() => {
+  
+    navigate('/');
+  }, [navigate]);
+
+  return null;    
+}
 
 function App() {
-
-  const navigate = useNavigate();
-  
-    useEffect(() => {
-      navigate('/login');
-    }, [navigate]);
-
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
-
       </Routes>
     </Router>
   );
